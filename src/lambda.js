c1c8a0d68a2ef6b7e778d322db3bd04bf05e6552
Ammar <ammar@WSO2s-MacBook-Pro.local>
2023-03-11 16:32:28 +0530

    git pipeline init

---
+++ src/lambda.js
@@ -0,0 +1,10 @@
+// lambda.js
+
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./dist/main');
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
